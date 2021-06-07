7fcd0909f5459d3e6c6e741823abc4f58eb1185b
Mohammad Alzamami <alzamami981@gmail.com>
2021-06-07 20:09:16 +0300

    finished day one

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import products from "./products";
 
 function App() {
+  const productsList = products.map((product) => (
+    <div className="productIMG" id={product.id}>
+      <img alt={product.name} src={product.image} className="UEFABall" />
+      <p className="text">{product.name}</p>
+      <p className="text">{product.price} KD</p>
+    </div>
+  ));
   return (
     <div>
       <h1 className="text">Zamami Sports</h1>
-      <h3 className="text">All of your needs in one place</h3>
+      <h3 className="text">All your needs in one place</h3>
       <img
         src="https://www.nelsonworldwide.com/wp-content/uploads/2019/09/dicks-sporting-goods-06.jpg"
-        className="shop-image"
+        className="shope-image"
+        alt="shop"
       />
+      <div className="list">{productsList}</div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "UEFA Ball",
+    price: 12,
+    image:
+      "https://www.rebelsport.com.au/dw/image/v2/BBRV_PRD/on/demandware.static/-/Sites-srg-internal-master-catalog/default/dwc622546c/images/59573601/Rebel_59573601_hi-res.jpg?sw=1000&sh=1000&sm=fit",
+  },
+  {
+    id: 2,
+    name: "Jumpping Rope",
+    price: 5,
+    image:
+      "https://www.dragonsports.eu/333581-verylarge_default/nylon-jumping-rope-grip-foam-adi1102-adidas.jpg",
+  },
+  {
+    id: 3,
+    name: "Boxing Gloves",
+    price: 11,
+    image:
+      "https://hayabusa-us.imgix.net/products/HayabusaCaptainAmericaBoxingGloves_MainImg_22f6b807-1e9c-4360-a44e-017d1ed48630.png?fm=pjpg&q=80&w=1200&h=1200&fit=fill&bg=F4F4F4&auto=compress,format",
+  },
+  {
+    id: 4,
+    name: "Boxing Gloves",
+    price: 11,
+    image:
+      "https://hayabusa-us.imgix.net/products/HayabusaCaptainAmericaBoxingGloves_MainImg_22f6b807-1e9c-4360-a44e-017d1ed48630.png?fm=pjpg&q=80&w=1200&h=1200&fit=fill&bg=F4F4F4&auto=compress,format",
+  },
+];
+
+export default products;
